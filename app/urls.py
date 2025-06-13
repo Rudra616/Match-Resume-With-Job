6a57cc607633cfdc3cb5586e1d95677071a31baf
rudra <rudrampanchal@gmail.com>
2025-06-13 19:49:43 +0530

    urls for app

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Make sure this is valid
+    path('', views.home, name='home'),  
+    path('find-job',views.find_job,name='find_job'),
+    path('create-job/', views.job_create, name='job_create'),
 ]
